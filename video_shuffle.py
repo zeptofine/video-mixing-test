fc1cb4c50b4999ba605208aa3c6bc2a7b8c1aca2
zeptofine <xpsychonauticonx@gmail.com>
2023-12-22 22:05:05 -0500

    handle a variable number of video streams

--- video_shuffle.py
+++ video_shuffle.py
@@ -30,35 +30,46 @@ with tempfile.TemporaryDirectory(suffix="video-mixing-test") as tmpdir:
 
     filedata = ffmpeg.probe(file)
     video_stream_data: dict | None = None
+    audio_streams = []
     for stream in filedata["streams"]:
         if stream["codec_type"] == "video":
             video_stream_data = stream
-            break
+        else:
+            audio_streams.append(stream)
     assert video_stream_data is not None
     numerator, denominator = video_stream_data["avg_frame_rate"].split("/")
     fps: float = float(numerator) / float(denominator)
     single_frame_duration = 1 / fps
 
     fi = ffmpeg.input(file)
-    print(f"separating audio to {audiopath}...")
-    (
-        fi.audio.output(str(audiopath), format="wav", ar="44100", loglevel="error")
-        .global_args("-hide_banner", "-stats")
-        .run()
-    )
+    from pprint import pprint
 
-    samplerate, data = wavread(audiopath)
-    total_length = len(data) / samplerate
-    samples_per_frame: float = samplerate / fps
-    audio_chunks = np.split(
-        data,
-        range(
-            int(samples_per_frame),
-            len(data),
-            int(samples_per_frame * split_size_frames),
-        ),
-        axis=0,
-    )
+    print(fi.video)
+    pprint(audio_streams)
+    all_audio = []
+    samplerate = 44100
+    for i, stream in enumerate(audio_streams):
+        filepath = audiopath.with_stem(f"audio_{i}")
+        print(f"separating audio stream {i} to {filepath}")
+        (
+            fi.audio.output(str(audiopath), format="wav", ar="44100", loglevel="error")
+            .global_args("-hide_banner", "-stats")
+            .run()
+        )
+
+        samplerate, data = wavread(audiopath)
+        total_length = len(data) / samplerate
+        samples_per_frame: float = samplerate / fps
+        audio_chunks = np.split(
+            data,
+            range(
+                int(samples_per_frame),
+                len(data),
+                int(samples_per_frame * split_size_frames),
+            ),
+            axis=0,
+        )
+        all_audio.append(audio_chunks)
 
     print(f"saving video frames to {tmpdir}...")
     fi.video.output(str(imagepaths), loglevel="error").global_args("-hide_banner", "-stats").run()
@@ -66,22 +77,31 @@ with tempfile.TemporaryDirectory(suffix="video-mixing-test") as tmpdir:
     video_chunks = (images[i : i + split_size_frames] for i in range(0, len(images), split_size_frames))
 
     # bind video and audio chunks, shuffle them, then split again
-    v_chunks_, a_chunks = zip(
+    v_chunks_, *a_chunks = zip(
         *sorted(
             zip(
                 video_chunks,
-                audio_chunks,
+                *all_audio,
             ),
             key=lambda _: random.random(),
         )
     )
+
     v_chunks = (y for x in v_chunks_ for y in x)  # [[a,b],[c,d]] -> [a,b,c,d]
-    shuffled_audio = np.vstack(a_chunks)
 
-    print(f"saving shuffled audio to {audiopath}...")
-    wavwrite(audiopath, samplerate, shuffled_audio)
+    shuffled_audios: list[np.ndarray] = [
+        np.vstack(a_chunk) if len(a_chunk[0].shape) != 1 else np.hstack(a_chunk) for a_chunk in a_chunks
+    ]
 
-    audioinput = ffmpeg.input(audiopath).audio
+    audiopaths = []
+    for i, stream in enumerate(shuffled_audios):
+        filepath = audiopath.with_stem(f"audio_{i}")
+        print(f"separating audio stream {i} to {filepath}")
+        wavwrite(filepath, samplerate, stream)
+        audiopaths.append(filepath)
+
+    pprint(shuffled_audios)
+
     # create new folder of symlinks with the files in order
     with tempfile.TemporaryDirectory(suffix="video-mixing-test-links") as linkdir:
         print(f"symlinking ordered images to {linkdir}...")
@@ -93,17 +113,18 @@ with tempfile.TemporaryDirectory(suffix="video-mixing-test") as tmpdir:
 
         print(f"saving new video to {output_path}...")
         # save the new file
-        
-        
-        x=(
+
+        x = (
             ffmpeg.output(
                 vidinput,
-                audioinput,
+                *[ffmpeg.input(pth).audio for pth in audiopaths],
                 str(output_path),
-                vcodec="h264",
             )
             .global_args("-hide_banner", "-stats")
             .overwrite_output()
         )
         print(ffmpeg.compile(x))
         x.run()
+
+    exit()
+
